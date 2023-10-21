9729044be0159994e6b4f418ccc4de6c826f543b
AlMamunTalukder <almamuntalukder0@gmail.com>
2023-10-21 21:49:24 +0600

    delete

--- index.js
+++ index.js
@@ -92,6 +92,15 @@ async function run() {
       res.send(result);
     });
 
+    //delete user or data from db
+    app.delete("/getCartData/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await myCartCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
